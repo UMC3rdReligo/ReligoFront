27a4591a8ee6ba9d742569f7d5efa80872720399
JjungminLee <julie0964@naver.com>
2023-01-23 18:49:03 +0900

    feat:bnb fragment 연결

---
+++ app/src/main/java/com/UMCfront/religo/CommunityFragment.kt
@@ -0,0 +1,27 @@
+package com.UMCfront.religo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CommunityFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_community, container, false)
+    }
+
+    companion object {
+        // fragment 인스턴스 사용하기 위해 companion object 사용
+        fun newInstance():CommunityFragment{
+            return CommunityFragment()
+        }
+    }
+}
