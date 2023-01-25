8abdce532ff68460dac64af46227e5e829ebc072
Eunse Jang <senuej37@gmai.com>
2023-01-25 19:23:36 +0900

    스플래시 화면, 유저 성향 파악 조사 화면 layout 구현

---
+++ app/src/main/java/com/UMCfront/religo/WelcomePageActivity.kt
@@ -0,0 +1,11 @@
+package com.UMCfront.religo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+    }
+}
