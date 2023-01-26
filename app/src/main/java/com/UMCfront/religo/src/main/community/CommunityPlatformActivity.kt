f812b5c473b4f1d50cb01044f5c8dd4bbe547329
JjungminLee <julie0964@naver.com>
2023-01-26 14:02:00 +0900

    feat: layout 변경및 community 상세페이지 구현

---
+++ app/src/main/java/com/UMCfront/religo/src/main/community/CommunityPlatformActivity.kt
@@ -0,0 +1,12 @@
+package com.UMCfront.religo.src.main.community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.UMCfront.religo.R
+
+class CommunityPlatformActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_platform)
+    }
+}
