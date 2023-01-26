35e5a64317edbacc16bce6334ae0a3444f2415b8
JjungminLee <julie0964@naver.com>
2023-01-27 02:01:20 +0900

    feat:community detail view

--- app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllActivity.kt
+++ app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllActivity.kt
@@ -6,16 +6,18 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.UMCfront.religo.R
+import com.UMCfront.religo.databinding.ActivityCommunityAllBinding
 
 import com.UMCfront.religo.src.main.community.adapter.CommunityGridAdapter
 
 class CommunityAllActivity : AppCompatActivity() {
 
 
+    private lateinit var binding: ActivityCommunityAllBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_community_all)
+        setContentView(R.layout.activity_community_all)
 
 
 
@@ -36,15 +38,18 @@ class CommunityAllActivity : AppCompatActivity() {
         communityAllList.add("안녕하세요 이번에 새로 가입했습니다.")
 
 
-//        binding= DataBindingUtil.setContentView(this,R.layout.activity_community_all)
-//
-//        val communityAdapter= CommunityGridAdapter(communityAllList)
-//
-//        //community_grid_rv
-//        val rv=binding.communityGridRv
-//        rv.adapter=communityAdapter
-//
-//
-//        rv.layoutManager= GridLayoutManager(this,2)
+        binding= ActivityCommunityAllBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val communityAdapter= CommunityGridAdapter(communityAllList)
+
+        //community_grid_rv
+        val rv=binding.communityGridRv
+        rv.adapter=communityAdapter
+
+
+        rv.layoutManager= GridLayoutManager(this,2)
+
+
     }
 }
