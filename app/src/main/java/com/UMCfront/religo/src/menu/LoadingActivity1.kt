30fae1beb192bc68747b4dbd1fa8789b8cc1bb5a
Eunse Jang <senuej37@gmai.com>
2023-01-27 00:16:44 +0900

    1/27 1차 구현

---
+++ app/src/main/java/com/UMCfront/religo/src/menu/LoadingActivity1.kt
@@ -0,0 +1,10 @@
+package com.UMCfront.religo.src.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
