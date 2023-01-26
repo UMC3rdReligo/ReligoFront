0f640efc265fa6799bb9d6b279465ed7186d3f12
JjungminLee <julie0964@naver.com>
2023-01-27 02:57:41 +0900

    feat:community floating

--- app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllActivity.kt
+++ app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllActivity.kt
@@ -15,6 +15,8 @@ class CommunityAllActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCommunityAllBinding
 
+    val fragment_community_writing=CommunityWritingFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_all)
@@ -49,6 +51,15 @@ class CommunityAllActivity : AppCompatActivity() {
 
 
         rv.layoutManager= GridLayoutManager(this,2)
+
+
+        // 글쓰기 버튼 구현
+        binding.communityAllFloating.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction().
+                replace(R.id.community_all,fragment_community_writing)
+                .commit()
+        }
 
 
     }
