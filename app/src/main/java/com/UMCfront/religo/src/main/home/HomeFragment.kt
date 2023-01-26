de95ce61c314b53a61a9982f3cc5a7419638fb91
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-01-26 14:01:00 +0900

    feat:home 화면 fragment 구현중

--- app/src/main/java/com/UMCfront/religo/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/UMCfront/religo/src/main/home/HomeFragment.kt
@@ -1,15 +1,22 @@
 package com.UMCfront.religo.src.main.home
 
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.UMCfront.religo.R
+import com.UMCfront.religo.databinding.FragmentHomeBinding
+import com.UMCfront.religo.src.main.home.adapter.PagerFragmentStateAdapter
 
 
 class HomeFragment : Fragment() {
+
+    private var viewPager: ViewPager2? = null
+
+
 
 
     override fun onCreateView(
@@ -20,7 +27,27 @@ class HomeFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val pagerAdapter = PagerFragmentStateAdapter(requireActivity())
+        // 3개의 Fragment Add
+        pagerAdapter.addFragment(HomeFirstFragment())
+        pagerAdapter.addFragment(HomeSecondFragment())
+        // Adapter
+        viewPager?.adapter = pagerAdapter
+
+        viewPager?.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.e("ViewPagerFragment", "Page ${position+1}")
+            }
+        })
+    }
+
 
 
 
