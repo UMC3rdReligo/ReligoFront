ba33cfda71477b8a8a6e0b2ce2a40f22123563ac
JjungminLee <julie0964@naver.com>
2023-01-27 16:20:34 +0900

    feat:커뮤니티 글쓰기 페이지

--- app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllFragment.kt
+++ app/src/main/java/com/UMCfront/religo/src/main/community/CommunityAllFragment.kt
@@ -1,30 +1,35 @@
 package com.UMCfront.religo.src.main.community
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.UMCfront.religo.R
 import com.UMCfront.religo.databinding.ActivityCommunityAllBinding
-
+import com.UMCfront.religo.databinding.FragmentCommunityAllBinding
+import com.UMCfront.religo.src.main.MainActivity
 import com.UMCfront.religo.src.main.community.adapter.CommunityGridAdapter
 
-class CommunityAllActivity : AppCompatActivity(){
 
+class CommunityAllFragment : Fragment() {
+    private var _binding: FragmentCommunityAllBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var binding: ActivityCommunityAllBinding
-
-
-    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_community_all)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
 
+        val binding = FragmentCommunityAllBinding.inflate(inflater, container, false)
 
         var communityAllList= mutableListOf<String>()
         communityAllList.add("안녕하세요 이번에 새로 가입했습니다.")
@@ -43,8 +48,7 @@ class CommunityAllActivity : AppCompatActivity(){
         communityAllList.add("안녕하세요 이번에 새로 가입했습니다.")
 
 
-        binding= ActivityCommunityAllBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
 
         val communityAdapter= CommunityGridAdapter(communityAllList)
 
@@ -53,7 +57,7 @@ class CommunityAllActivity : AppCompatActivity(){
         rv.adapter=communityAdapter
 
 
-        rv.layoutManager= GridLayoutManager(this,2)
+        rv.layoutManager= GridLayoutManager(context,2)
 
         binding.communityFab.bringToFront()
 
@@ -61,15 +65,20 @@ class CommunityAllActivity : AppCompatActivity(){
 
         val fragment_community_writing=CommunityWritingFragment()
         binding.communityFab.setOnClickListener{
-            Toast.makeText(applicationContext,"플로팅 클릭",Toast.LENGTH_LONG).show()
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.community_all,fragment_community_writing)
-                .commit()
+
+            Toast.makeText(context,"플로팅 클릭", Toast.LENGTH_LONG).show()
+
+            (activity as MainActivity?)?.changeFragment(CommunityWritingFragment())
+
         }
 
+        return binding.root
 
     }
 
-
+    companion object {
+        fun newInstance(): CommunityAllFragment {
+            return CommunityAllFragment()
+        }
+    }
 }
