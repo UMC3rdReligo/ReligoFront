043d37fccd266f794a1caa8c7851febb7e70cd40
JjungminLee <julie0964@naver.com>
2023-01-30 10:13:16 +0900

    fix:menu5

--- app/src/main/java/com/UMCfront/religo/src/menu/MenuActivity5.kt
+++ app/src/main/java/com/UMCfront/religo/src/menu/MenuActivity5.kt
@@ -8,5 +8,6 @@ class MenuActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu5)
+        //
     }
 }
