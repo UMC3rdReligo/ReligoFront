3e52f29305b32e5d46b94612fddfa4dede6bab04
JjungminLee <julie0964@naver.com>
2023-01-30 09:37:12 +0900

    fix: menu activity5 문제 해결

--- app/src/main/java/com/UMCfront/religo/src/menu/MenuActivity5.kt
+++ app/src/main/java/com/UMCfront/religo/src/menu/MenuActivity5.kt
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.UMCfront.religo.R
 
-class SplashActivity : AppCompatActivity() {
+class MenuActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
+        setContentView(R.layout.activity_menu5)
     }
 }
